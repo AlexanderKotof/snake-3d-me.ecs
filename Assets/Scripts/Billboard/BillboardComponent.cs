d035934fb7117c7a4fb7a1d5d46084afb6ecf14c
Alexander Kotov <newbeedevinc@gmail.com>
2023-07-24 11:29:40 +0400

    Fix

--- Assets/Scripts/Billboard/BillboardComponent.cs
+++ Assets/Scripts/Billboard/BillboardComponent.cs
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:901e4968bd8d2888efcf35be00765eff61da8aa05b89581e8f966d156f253f9e
-size 279
+using UnityEngine;
+
+public class BillboardComponent : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.forward);
+    }
+}
